fe21c57616353f3d4be46bf135c4c1211b429778
Artur <artur.jesslen@gmail.com>
2019-10-22 13:52:27 +0200

    lab5

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -6,12 +6,15 @@ Least Square
 
 import numpy as np
 
+def compute_error(y, tx, w):
+    return y - np.dot(tx, w)
+
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    return 1 / 2 * np.mean(e ** 2)
 
 def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    """Least squares normal equations."""
+    w = np.linalg.solve(np.dot(tx.T, tx), np.dot(tx.T, y))
+    loss = calculate_mse(compute_error(y, tx, w))
+    return w
